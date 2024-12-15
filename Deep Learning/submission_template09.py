305d1d24379ee76e73b29b344c755a61d4a5a999
Hellover1 <kowarin.samovarin@gmail.com>
2024-12-15 20:42:05 +0300

    submission_template09.py

--- Deep Learning/submission_template09.py
+++ Deep Learning/submission_template09.py
@@ -22,18 +22,9 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # Преобразуем состояния энкодера с помощью матрицы весов W_mult
-    transformed_encoder_states = np.dot(W_mult, encoder_hidden_states)
-
-    # Вычисляем скалярное произведение между состоянием декодера и каждым состоянием энкодера
-    attention_scores = np.dot(decoder_hidden_state.T, transformed_encoder_states)
-
-    # Применяем softmax для получения весов
-    weights_vector = softmax(attention_scores)
-
-    # Вычисляем итоговый attention vector как взвешенную сумму состояний энкодера
-    attention_vector = weights_vector.dot(encoder_hidden_states.T).T
-
+    # your code here
+    softmax_vector = softmax(decoder_hidden_state.T @ W_mult @ encoder_hidden_states)
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
     return attention_vector
 
 def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
@@ -47,61 +38,8 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # Преобразуем состояния энкодера и декодера с помощью весовых матриц
-    transformed_encoder_states = np.dot(W_add_enc, encoder_hidden_states)
-    transformed_decoder_state = np.dot(W_add_dec, decoder_hidden_state)
-
-    # Суммируем результаты и применяем tanh
-    combined_states = np.tanh(transformed_encoder_states + transformed_decoder_state)
-
-    # Применяем скалярное произведение с вектором весов v_add
-    attention_scores = np.dot(v_add.T, combined_states)
-
-    # Применяем softmax для получения весов
-    weights_vector = softmax(attention_scores)
-
-    # Вычисляем итоговый attention vector как взвешенную сумму состояний энкодера
-    attention_vector = weights_vector.dot(encoder_hidden_states.T).T
-
+    # your code here
+    softmax_vector = softmax(v_add.T @ np.tanh(W_add_enc @ encoder_hidden_states + W_add_dec @ decoder_hidden_state))
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
     return attention_vector
 
-# Проверка работы для additive attention
-v_add = np.array([[-0.35, -0.58,  0.07,  1.39, -0.79, -1.78, -0.35]]).T
-
-W_add_enc = np.array([
-    [-1.34, -0.1 , -0.38,  0.12, -0.34],
-    [-1.  ,  1.28,  0.49, -0.41, -0.32],
-    [-0.39, -1.38,  1.26,  1.21,  0.15],
-    [-0.18,  0.04,  1.36, -1.18, -0.53],
-    [-0.23,  0.96,  1.02,  0.39, -1.26],
-    [-1.27,  0.89, -0.85, -0.01, -1.19],
-    [ 0.46, -0.12, -0.86, -0.93, -0.4 ]
-])
-
-W_add_dec = np.array([
-    [-1.62, -0.02, -0.39],
-    [ 0.43,  0.61, -0.23],
-    [-1.5 , -0.43, -0.91],
-    [-0.14,  0.03,  0.05],
-    [ 0.85,  0.51,  0.63],
-    [ 0.39, -0.42,  1.34],
-    [-0.47, -0.31, -1.34]
-])
-
-encoder_hidden_states_complex = np.array([
-    [1, 5, 11, 4, -4],
-    [7, 4, 1, 2, 2],
-    [8, 12, 2, 11, 5],
-    [-9, 0, 1, 8, 12]
-]).astype(float).T
-
-decoder_hidden_state = np.array([[1], [0.5], [-1]])
-
-attention_vector = additive_attention(decoder_hidden_state, encoder_hidden_states_complex, v_add, W_add_enc, W_add_dec)
-print("Additive Attention Vector:")
-print(attention_vector)
-
-plt.figure(figsize=(2, 5))
-plt.pcolormesh(attention_vector, cmap='cool')
-plt.colorbar()
-plt.show()
