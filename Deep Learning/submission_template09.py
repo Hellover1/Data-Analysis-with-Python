842ba6ae2d6b3275ec1351714f4ca34604669234
Hellover1 <kowarin.samovarin@gmail.com>
2024-12-19 14:47:43 +0300

    Update submission_template09.py

--- Deep Learning/submission_template09.py
+++ Deep Learning/submission_template09.py
@@ -1,60 +1,65 @@
-iimport numpy as np
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 
-def encoder_block(in_channels, out_channels, kernel_size=3, padding=1):
+def encoder_block(in_channels, out_channels, kernel_size, padding):
     '''
-    Создает блок энкодера: conv -> relu -> max_pooling
+    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    и выдает на выход карты активации с количеством каналов out_channels
+    kernel_size, padding — параметры conv слоев внутри блока
     '''
-    return nn.Sequential(
+    block = nn.Sequential(
         nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-        nn.ReLU(),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2)
     )
 
-def decoder_block(in_channels, out_channels, kernel_size=3, padding=1):
+    return block
+
+def decoder_block(in_channels, out_channels, kernel_size, padding):
     '''
-    Создает блок декодера: conv -> relu -> upsample
+    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    и выдает на выход карты активации с количеством каналов out_channels
+    kernel_size, padding — параметры conv слоев внутри блока
     '''
-    return nn.Sequential(
+    block = nn.Sequential(
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
         nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-        nn.ReLU(),
-        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
     )
 
-class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    return block
+
+class Autoencoder(nn.Module):
+    def __init__(self):
         super().__init__()
 
-        self.enc1_block = encoder_block(in_channels, 32, 7, 3)
-        self.enc2_block = encoder_block(32, 64, 3, 1)
-        self.enc3_block = encoder_block(64, 128, 3, 1)
+        # Добавляем слои энкодера
+        self.encoder = nn.Sequential(
+            encoder_block(3, 64, kernel_size=3, padding=1),
+            encoder_block(64, 128, kernel_size=3, padding=1),
+            encoder_block(128, 256, kernel_size=3, padding=1)
+        )
 
-        self.dec1_block = decoder_block(128, 64, 3, 1)
-        self.dec2_block = decoder_block(64 + 64, 32, 3, 1)
-        self.dec3_block = decoder_block(32 + 32, out_channels, 3, 1)
+        # Добавляем слои декодера
+        self.decoder = nn.Sequential(
+            decoder_block(256, 128, kernel_size=3, padding=1),
+            decoder_block(128, 64, kernel_size=3, padding=1),
+            nn.Conv2d(64, 3, kernel_size=3, padding=1),
+            nn.Tanh()  # Для восстановления значений в диапазоне [-1, 1]
+        )
 
     def forward(self, x):
-        # downsampling part
-        enc1 = self.enc1_block(x)
-        enc2 = self.enc2_block(enc1)
-        enc3 = self.enc3_block(enc2)
+        # Downsampling
+        latent = self.encoder(x)
 
-        dec1 = self.dec1_block(enc3)
-        # из-за skip connection dec2 должен принимать на вход сконкатенированные карты активации
-        # из блока dec1 и из блока enc2. 
-        # конкатенация делается с помощью torch.cat
-        dec2 = self.dec2_block(torch.cat([dec1, enc2], dim=1))
-        # из-за skip connection dec3 должен принимать на вход сконкатенированные карты активации
-        # из блока dec2 и из блока enc1. 
-        # конкатенация делается с помощью torch.cat
-        dec3 = self.dec3_block(torch.cat([dec2, enc1], dim=1))
+        # Upsampling
+        reconstruction = self.decoder(latent)
 
-        return dec3
+        return reconstruction
 
-def create_model(in_channels, out_channels):
-    '''
-    Функция для создания модели UNet
-    '''
-    return UNet(in_channels, out_channels)
+def create_model():
+    return Autoencoder()
