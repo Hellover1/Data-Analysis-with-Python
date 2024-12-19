71e274cff5994e3e37ba2ca3fa0b67b763ebdfd7
Hellover1 <kowarin.samovarin@gmail.com>
2024-12-19 13:26:11 +0300

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -1,106 +1,81 @@
+import numpy as np
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
+from torch.nn import functional as F
 
-# Устройство: CPU или GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
+def encoder_block(in_channels, out_channels, kernel_size, padding):
+    '''
+    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    и выдает на выход карты активации с количеством каналов out_channels
+    kernel_size, padding — параметры conv слоев внутри блока
+    '''
 
-# Определение UNet
+    # Реализуйте блок вида conv -> relu -> max_pooling. 
+    # Параметры слоя conv заданы параметрами функции encoder_block. 
+    # MaxPooling должен быть с ядром размера 2.
+    block = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2)
+    )
+
+    return block
+
+def decoder_block(in_channels, out_channels, kernel_size, padding):
+    '''
+    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    и выдает на выход карты активации с количеством каналов out_channels
+    kernel_size, padding — параметры conv слоев внутри блока
+    '''
+
+    # Реализуйте блок вида conv -> relu -> upsample. 
+    # Параметры слоя conv заданы параметрами функции encoder_block. 
+    # Upsample должен быть со scale_factor=2. Тип upsampling (mode) можно выбрать любым.
+    block = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2.0, mode='bilinear', align_corners=True)
+    )
+
+    return block
+
 class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(UNet, self).__init__()
-        self.encoder1 = self.encoder_block(in_channels, 64)
-        self.encoder2 = self.encoder_block(64, 128)
-        self.encoder3 = self.encoder_block(128, 256)
-        self.encoder4 = self.encoder_block(256, 512)
-        
-        self.decoder4 = self.decoder_block(512, 256)
-        self.decoder3 = self.decoder_block(256 + 256, 128)
-        self.decoder2 = self.decoder_block(128 + 128, 64)
-        self.decoder1 = nn.Conv2d(64 + 64, out_channels, kernel_size=1)
+        '''
+        параметры: 
+            - in_channels: количество каналов входного изображения
+            - out_channels: количество каналов выхода нейросети
+        '''
+        super().__init__()
 
-    def encoder_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2)
-        )
+        self.enc1_block = encoder_block(in_channels, 32, 7, 3)
+        self.enc2_block = encoder_block(32, 64, 3, 1)
+        self.enc3_block = encoder_block(64, 128, 3, 1)
 
-    def decoder_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Upsample(scale_factor=2)
-        )
+        self.dec1_block = decoder_block(128, 64, 3, 1)
+        self.dec2_block = decoder_block(64 + 64, 32, 3, 1)
+        self.dec3_block = decoder_block(32 + 32, out_channels, 3, 1)
 
     def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(enc1)
-        enc3 = self.encoder3(enc2)
-        enc4 = self.encoder4(enc3)
+        # downsampling part
+        enc1 = self.enc1_block(x)
+        enc2 = self.enc2_block(enc1)
+        enc3 = self.enc3_block(enc2)
 
-        dec4 = self.decoder4(enc4)
-        dec3 = self.decoder3(torch.cat([dec4, enc3], dim=1))
-        dec2 = self.decoder2(torch.cat([dec3, enc2], dim=1))
-        dec1 = self.decoder1(torch.cat([dec2, enc1], dim=1))
-        return dec1
+        dec1 = self.dec1_block(enc3)
+        # из-за skip connection dec2 должен принимать на вход сконкатенированные карты активации
+        # из блока dec1 и из блока enc2. 
+        # конкатенация делается с помощью torch.cat
+        dec2 = self.dec2_block(torch.cat([dec1, enc2], 1))
+        # из-за skip connection dec3 должен принимать на вход сконкатенированные карты активации
+        # из блока dec2 и из блока enc1. 
+        # конкатенация делается с помощью torch.cat
+        dec3 = self.dec3_block(torch.cat([dec2, enc1], 1))
 
-# Подготовка данных
-train_data = torch.rand(50, 3, 128, 128)  # Пример входных данных (3 канала)
-train_targets = torch.randint(0, 5, (50, 128, 128))  # Пример сегментационных масок (5 классов)
+        return dec3
 
-val_data = torch.rand(10, 3, 128, 128)
-val_targets = torch.randint(0, 5, (10, 128, 128))
+def create_model(in_channels, out_channels):
+    # your code here
+    # return model instance (None is just a placeholder)
 
-train_loader = DataLoader(TensorDataset(train_data, train_targets), batch_size=8, shuffle=True)
-val_loader = DataLoader(TensorDataset(val_data, val_targets), batch_size=8)
-
-# Параметры модели
-model = UNet(in_channels=3, out_channels=5).to(device)
-loss = nn.CrossEntropyLoss()
-opt = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-# Функция обучения
-def train(model, opt, loss_fn, epochs, train_loader, val_loader):
-    for epoch in range(epochs):
-        print(f'* Epoch {epoch + 1}/{epochs}')
-        
-        # Training
-        model.train()
-        avg_train_loss = 0
-        for X_batch, Y_batch in train_loader:
-            X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
-            opt.zero_grad()
-            Y_pred = model(X_batch)
-            loss = loss_fn(Y_pred, Y_batch.long())
-            loss.backward()
-            opt.step()
-            avg_train_loss += loss.item() / len(train_loader)
-        print(f'avg train loss: {avg_train_loss:.4f}')
-
-        # Validation
-        model.eval()
-        avg_val_loss = 0
-        with torch.no_grad():
-            for X_batch, Y_batch in val_loader:
-                X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
-                Y_pred = model(X_batch)
-                loss = loss_fn(Y_pred, Y_batch.long())
-                avg_val_loss += loss.item() / len(val_loader)
-        print(f'avg val loss: {avg_val_loss:.4f}')
-
-# Запуск обучения
-train(model, opt, loss, 10, train_loader, val_loader)
-
+    return UNet(in_channels, out_channels)
